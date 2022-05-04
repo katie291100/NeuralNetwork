d38d20b4dc22aa128d3cbec49f190b7b79f97b8f
katie291100 <kmm565@york.ac.uk>
2022-05-04 18:08:36 +0100

    add colour jitter, 83% after 40 epochs but tailing off

--- 85_after_28_epochs (1).py
+++ 85_after_28_epochs (1).py
@@ -1,14 +1,7 @@
-import torch # idk which imports are needed in this barebones version
+import torch  # idk which imports are needed in this barebones version
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-from captum.attr import IntegratedGradients
-from captum.attr import LayerConductance
-from captum.attr import NeuronConductance
-from captum.insights import AttributionVisualizer, Batch
-from captum.insights.attr_vis.features import ImageFeature
-import matplotlib.pyplot as plt
-import numpy as np
 import time
 
 
@@ -17,49 +10,45 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
 
         self.conv1 = nn.Conv2d(3, 128, 3, 1)
-        self.conv2 = nn.Conv2d(128, 256, 3, 1)
-        self.conv3 = nn.Conv2d(256, 256, 3, 1, padding="same")
-        self.conv4 = nn.Conv2d(256, 512, 3, 1)
-        self.conv5 = nn.Conv2d(512, 512, 3, 1, padding="same")
+        self.conv2 = nn.Conv2d(128, 256, 3, 1, padding=2)
+        self.conv3 = nn.Conv2d(256, 256, 3, 1, padding=2)
+        self.conv4 = nn.Conv2d(256, 512, 3, 1, padding=1)
+        self.conv5 = nn.Conv2d(512, 512, 3, 1, padding=1)
         self.conv6 = nn.Conv2d(512, 1024, 3, 1)
+        self.conv7 = nn.Conv2d(1024, 1024, 3, 1)
 
-        self.maxPool = nn.MaxPool2d(2)
+        self.maxPool = nn.MaxPool2d(2, 2)
 
         self.drop1 = nn.Dropout(0.3)
-        self.drop2 = nn.Dropout(0.4)
+        self.drop2 = nn.Dropout(0.3)
+        self.drop3 = nn.Dropout(0.2)
+        self.drop4 = nn.Dropout(0.4)
 
-        self.fc1 = nn.Linear(4096, 8192)
-        self.fc2 = nn.Linear(8192, 4096)
+        self.fc1 = nn.Linear(1024, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
         self.fc3 = nn.Linear(4096, 10)
 
     def forward(self, x):
-        # i = 0
-        # f = lambda i: [i+1,print(i,x.shape)][0]
+        i = 0
+        f = lambda i: [i + 1, print(i, x.shape)][0]  # i = f(i) will print the shape of x
 
         x = nn.functional.rrelu(self.conv1(x))
+        x = self.drop1(x)
+        x = self.maxPool(x)
         x = nn.functional.rrelu(self.conv2(x))
+        x = self.drop2(x)
         x = self.maxPool(x)
-
-        skip = x.clone()
-
         x = nn.functional.rrelu(self.conv3(x))
-        x = x + skip
-        x = self.drop1(x)
         x = nn.functional.rrelu(self.conv4(x))
+        x = self.drop3(x)
         x = self.maxPool(x)
-
-        skip = x.clone()
-
         x = nn.functional.rrelu(self.conv5(x))
-        x = x + skip
-        x = self.drop1(x)
         x = nn.functional.rrelu(self.conv6(x))
-        x = self.maxPool(x)
-
-        x = x.reshape(x.shape[0], -1)
-
+        x = self.drop4(x)
+        x = nn.functional.rrelu(self.conv7(x))
         # i=f(i)
-       # print(x.shape)
+        x = x.reshape(x.shape[0], -1)
+        # i=f(i)
         x = nn.functional.rrelu(self.fc1(x))
         x = nn.functional.rrelu(self.fc2(x))
         x = self.fc3(x)
@@ -68,13 +57,13 @@ class CNN(nn.Module):
 
 
 if __name__ == "__main__":
-    if torch.cuda.is_available(): # use gpu if possible
+    if torch.cuda.is_available():  # use gpu if possible
         device = torch.device("cuda")
-        print("cuda)")
     else:
         device = torch.device("cpu")
 
     model = CNN().to(device)
+
 
     def test():
         print("Testing")
@@ -92,64 +81,55 @@ if __name__ == "__main__":
             samples += labels.size(0)
             correct += (predictions == labels).sum()
 
-        print("Test accuracy was",100*float(correct)/float(samples))
+        print("Test accuracy was", 100 * float(correct) / float(samples))
         print()
 
 
-    epochs = 400
-    batch_size = 12
+    epochs = 40
+    batch_size = 16
     learning_rate = 0.0001
 
-    transform = transforms.Compose([
-        transforms.Resize(64),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4915, 0.4823, .4468), (0.2470, 0.2435, 0.2616))
-    ])
-    transformtrain = transforms.Compose([transforms.Resize((32,32)),  #resises the image so it can be perfect for our model.
-                                      transforms.RandomHorizontalFlip(), # FLips the image w.r.t horizontal axis
-                                      transforms.RandomRotation(10),     #Rotates the image to a specified angel
-                                      transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)), #Performs actions like zooms, change shear angles.
-                                      transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # Set the color params
-                                      transforms.ToTensor(), # comvert the image to tensor so that it can work with torch
-                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #Normalize all the images
-                               ])
+    train_transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.RandomHorizontalFlip(),
+                                    torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.5,
+                                                                             hue=0.3),
+                                    transforms.Normalize((0.5, 0.5, 0.5),
+                                                         (0.5, 0.5, 0.5))])
+
+    test_transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5),
+                                                         (0.5, 0.5, 0.5))])
+
+
 
     # get training/test data from CIFAR10 dataset
-    train_data = torchvision.datasets.CIFAR10(root = "./dataset",
-                                              train = True,
-                                              transform = transformtrain,
-                                              download = True)
-    test_data = torchvision.datasets.CIFAR10(root = "./dataset",
-                                             train = False,
-                                             transform = transformtrain,
-                                             download = True)
-    train_loader = torch.utils.data.DataLoader(dataset = train_data,
-                                               batch_size = batch_size,
-                                               shuffle = True,
-                                               num_workers = 4)
-    test_loader = torch.utils.data.DataLoader(dataset = test_data,
-                                              batch_size = batch_size,
-                                              shuffle = False,
-                                              num_workers = 4)
+    train_data = torchvision.datasets.CIFAR10(root="./dataset",
+                                              train=True,
+                                              transform=train_transform,
+                                              download=True)
+    test_data = torchvision.datasets.CIFAR10(root="./dataset",
+                                             train=False,
+                                             transform=test_transform,
+                                             download=True)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=2)
+    test_loader = torch.utils.data.DataLoader(dataset=test_data,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              num_workers=2)
 
-
-    found_lr = 1e-2
-
-    # try other loss functions/optimizers
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam([
-      { 'params': model.conv4.parameters(), 'lr': found_lr /3},
-      { 'params': model.conv3.parameters(), 'lr': found_lr /9},
-      ], lr=found_lr)
+    optimizer = torch.optim.Adam(model.parameters(), learning_rate, weight_decay=0.0015)
 
-    unfreeze_layers = [model.conv3, model.conv4]
-    for layer in unfreeze_layers:
-        for param in layer.parameters():
-            param.requires_grad = True
+    #optimizer = optimizer.Adam([{ 'params': transfer_model.layer4.parameters(), 'lr': found_lr /3}, { 'params': transfer_model.layer3.parameters(), 'lr': found_lr /9},], lr=found_lr)
+
     test_per_epoch = True
-    train_for_time = 0 # how many minutes to train for (and then finish current epoch)
+    train_for_time = 0  # how many minutes to train for (and then finish current epoch)
+
     if train_for_time:
-      epochs = train_for_time*1000
+        epochs = train_for_time * 1000
     start = time.time()
 
     for epoch in range(epochs):
@@ -171,14 +151,18 @@ if __name__ == "__main__":
 
             epoch_loss += loss.item()
 
-        print("Epoch", epoch+1, "complete")
-        print("Loss was", epoch_loss/len(train_loader))
+        print("Epoch", epoch + 1, "complete")
+        print("Loss was", epoch_loss / len(train_loader))
         print()
         if test_per_epoch:
             test()
 
-        if train_for_time and time.time()-start >= train_for_time*60:
+        if train_for_time and time.time() - start >= train_for_time * 60:
             break
 
     if not test_per_epoch:
         test()
+
+
+
+
